7bd1c3492f46500ff63083fcc3997da5960b7104
johnkingkong <jeanre002@gmail.com>
2022-10-27 12:28:35 -0400

    floodfill done

--- src/parsing/flood_fill.c
+++ src/parsing/flood_fill.c
@@ -6,7 +6,7 @@
 /*   By: anonymous <anonymous@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 16:03:18 by anonymous         #+#    #+#             */
-/*   Updated: 2022/10/26 20:13:08 by anonymous        ###   ########.fr       */
+/*   Updated: 2022/10/27 12:26:08 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,12 @@
 
 static bool	check_up(char **map, int i, int j)
 {
-	while(ft_isspace(map[i][j]))
-		j++;
-	while (i >= 0 && map[i][j])
+	while (map[i][j] && i >= 0)
 	{
 		if (map[i][j] == '1')
 			return (true);
-		if (map[i][j] == ' ' && map[i - 1] && map[i - 1][j]
-			&& map[i - 1][j] != '1')
-			return (false);
+		if (map[i][j] == ' ' || i < 1)
+			break ;
 		i--;
 	}
 	return (false);
@@ -33,55 +30,62 @@ static bool	check_down(char **map, int i, int j)
 	int	size;
 
 	size = len_dbl_array(map);
-	while(ft_isspace(map[i][j]))
-		j++;
-	while (i < size  && map[i][j])
+	while (map[i][j] && i < size)
 	{
 		if (map[i][j] == '1')
 			return (true);
-		if (map[i][j] == ' ' && map[i + 1] && map[i + 1][j]
-			&& map[i + 1][j] != '1')
-			return (false);
+		if (map[i][j] == ' ')
+			break ;
 		i++;
 	}
 	return (false);
 }
 
-/*static bool	check_right(char **map, int i, int j)
+static bool	check_right(char **map, int i, int j)
 {
-	while(ft_isspace(*map[i]))
-		map[i]++;
 	while (map[i][j])
 	{
-		if (map[i][j] == ' ' && map[i][j + 1] && map[i][j + 1] != '1')
-			return (false);
 		if (map[i][j] == '1')
 			return (true);
-		i++;
+		if (map[i][j] == ' ')
+			break ;
+		j++;
 	}
 	return (false);
 }
-*/
+
+static bool	check_left(char **map, int i, int j)
+{
+	while (map[i][j] && j >= 0)
+	{
+		if (map[i][j] == '1')
+			return (true);
+		if (map[i][j] == ' ')
+			break ;
+		j--;
+	}
+	return (false);
+}
+
 bool	flood_fill(void)
 {
 	t_map_infos	*infos;
 	int			i;
 	int			j;
-	char	**map;
 
 	infos = get_infos();
 	i = 0;
-	map = ft_cpy_dbl_arr(infos->map);
 	while (infos->map[i])
 	{
 		j = 0;
-		while (map[i][j])
+		while (infos->map[i][j])
 		{
-			if (map[i][j] == '\n')
-				break ;
-			if (!check_up(map, i, j) || !check_down(map, i, j)
-				|| !check_right(map, i, j))
-				error_exit("invalid_map");
+			if (infos->map[i][j] == '0')
+			{
+				if (!check_up(infos->map, i, j) || !check_down(infos->map, i, j)
+					|| !check_right(infos->map, i, j) || !check_left(infos->map, i, j))
+						error_exit("invalid_map");
+			}
 			j++;
 		}
 		i++;
